96e9bd8e62dada92e1644faee3b803e8b389889a
KhalidMujahid <binkhalid267@gmail.com>
2023-12-02 23:35:41 +0100

    change multer

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,16 +1,4 @@
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-  destination: (req, file, cb) => {
-    cb(null, "./public/assets");
-  },
-});
-
-module.exports.upload = multer({ storage });
+module.exports.upload = multer({ dest: "./public/assets" });
