f6f712cfb18a7220208120cdbe6a43855eaf58c7
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-01-19 08:50:18 +0100

    Update checkout.route.js

--- routes/checkout.route.js
+++ routes/checkout.route.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 
-const { returnPrice } = require("../controllers/checkout.controller");
+const { returnPrice,returnCheck } = require("../controllers/checkout.controller");
 
 const checkOutRouter = Router();
 
 // 1
 checkOutRouter.post("/price", returnPrice);
+
+
+// 5
+checkOutRouter.get("/price:/:id", returnCheck);
 
 // 2
 // checkOutRouter.post("/pay",);
