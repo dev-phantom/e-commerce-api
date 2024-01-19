230f62d6fa95edbe8caa82d47c47570fc8508df5
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-01-19 09:08:16 +0100

    Update checkout.controller.js

--- controllers/checkout.controller.js
+++ controllers/checkout.controller.js
@@ -22,7 +22,7 @@ async function returnPrice(req,res,next){
 
 async function returnCheck(req,res,next){
 	const { id } = req.query;
-	const checkouts = await CheckOut.find();
+	const checkouts = await CheckOut.find({ _id: id });
 	const { products,...others } = checkouts._doc;
 	return res.status(200).send(others);
 }
