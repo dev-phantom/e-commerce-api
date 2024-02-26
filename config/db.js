90e15053abef795b1544b460310b7fa3a30320c9
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-02-26 17:37:10 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-const mongoose,{ connect } = require("mongoose");
-mongoose.set("strictQuery",false);
+const { connect } = require("mongoose");
+//mongoose.set("strictQuery",false);
 
 function connectDB(){
   return connect(process.env.MONGO_DB)
