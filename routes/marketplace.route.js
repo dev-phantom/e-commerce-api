fd30f0eff72880f4eee3a4f7df513142f976db0f
KhalidMujahid <binkhalid267@gmail.com>
2024-03-26 06:56:21 +0100

    make changes again

--- routes/marketplace.route.js
+++ routes/marketplace.route.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { addMarket, addDistributor, getAllMarkets, getAllDistributors, deleteDistributor, deleteMarket, getDistributorsById } = require("../controllers/marketplace.controller");
+const { addMarket, addDistributor,deleteCity,editCity,getAllMarkets, getAllDistributors, deleteDistributor, deleteMarket, getDistributorsById } = require("../controllers/marketplace.controller");
 
 const MarketplaceRouter = Router();
 
@@ -17,5 +17,12 @@ MarketplaceRouter.post("/get-distributor", getDistributorsById)
 MarketplaceRouter.delete("/delete-distributors",deleteDistributor);
 // to get market
 MarketplaceRouter.delete("/delete-market",deleteMarket);
+
+//delete city
+MarketplaceRouter.delete("/delete/:id",deleteCity);
+
+//edit city
+MarketplaceRouter.patch("/edit",editCity);
+
 
 module.exports = MarketplaceRouter;
