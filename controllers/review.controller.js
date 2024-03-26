b4d08ee13317bf4f60a23fb0e6f9d0f6dbd023bd
KhalidMujahid <binkhalid267@gmail.com>
2024-03-26 06:43:40 +0100

    make changes

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,8 +1,14 @@
+const Review = require("../models/Review");
 
-module.exports.reviewRate = (req,res,next) => {
+module.exports.reviewRate = async (req,res,next) => {
 	try{
 		const { rate,comment } = req.body;
-		
+
+		if(!rate || !comment) return res.status(422).send({ message: "All fields are required!"});
+
+		const saved = await Review.create({ rate, comment });
+		if(saved) return res.status(200).send({ message: "Review sent!"});
+		else return res.status(400).send({ message: "An error occured!"});
 	} catch(e){
 		next(e);
 	}
