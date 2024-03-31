7c8d1aeefa5d8daa90e4f6c685ea7df06295a754
dev-phantom <godsfavour1975@gmail.com>
2024-03-31 12:21:34 +0100

    fix and added some endpoint

--- routes/marketplace.route.js
+++ routes/marketplace.route.js
@@ -1,22 +1,33 @@
 const { Router } = require("express");
-const { addMarket, addDistributor,getAllMarkets, getAllDistributors, deleteDistributor, deleteMarket, getDistributorsById } = require("../controllers/marketplace.controller");
+const { addMarket, addDistributor, getAllMarkets, getAllDistributors, deleteDistributor, deleteMarket, getDistributorsById, editMarket, getMarketById } = require("../controllers/marketplace.controller");
 
 const MarketplaceRouter = Router();
 
-// to add market
-MarketplaceRouter.post("/add-market",addMarket);
-// to get market
-MarketplaceRouter.get("/market",getAllMarkets);
-// to add market
-MarketplaceRouter.post("/add-distributor",addDistributor);
-// to get market
-MarketplaceRouter.get("/distributors",getAllDistributors);
-//get single distributor
-MarketplaceRouter.post("/get-distributor", getDistributorsById)
-// to get market
-MarketplaceRouter.delete("/delete-distributors",deleteDistributor);
-// to get market
-MarketplaceRouter.delete("/delete-market",deleteMarket);
+// Add a market
+MarketplaceRouter.post("/add-market", addMarket);
 
+// Get all markets
+MarketplaceRouter.get("/market", getAllMarkets);
+
+// Get a single market by ID
+MarketplaceRouter.get("/market/:id", getMarketById);
+
+// Add a distributor
+MarketplaceRouter.post("/add-distributor", addDistributor);
+
+// Get all distributors
+MarketplaceRouter.get("/distributors", getAllDistributors);
+
+// Get a single distributor by ID
+MarketplaceRouter.post("/get-distributor", getDistributorsById);
+
+// Update a market
+MarketplaceRouter.put("/market", editMarket);
+
+// Delete a distributor
+MarketplaceRouter.delete("/delete-distributors", deleteDistributor);
+
+// Delete a market
+MarketplaceRouter.delete("/delete-market", deleteMarket);
 
 module.exports = MarketplaceRouter;
